be77e139e29439893d5a533282ceac29cd3bf78c
Janik Lehmann <xxthegeckoxx@gmail.com>
2020-12-04 20:32:26 +0100

    improofed the raspi toolchain to match the manual

--- toolchains/toolchain-raspi.cmake
+++ toolchains/toolchain-raspi.cmake
@@ -1,6 +1,14 @@
 # https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-linux
 
-set(CMAKE_SYSROOT /home/janik/sysroot_local) #set the sysroot of your Raspberry Pi here
+if(NOT DEFINED CMAKE_SYSROOT)
+    if(EXISTS $ENV{HOME}/sysroot_raspi_local/)
+        set(CMAKE_SYSROOT $ENV{HOME}/sysroot_raspi_local) #set the sysroot of your Raspberry Pi here
+    elseif(EXISTS $ENV{HOME}/sysroot_raspi/)
+        set(CMAKE_SYSROOT $ENV{HOME}/sysroot_raspi) #set the sysroot of your Raspberry Pi here
+    else()
+        message( FATAL_ERROR "You have to set the sysroot of the Raspberry Pi with the command -DCMAKE_SYSROOT=PATH_TO_SYSROOT" )
+    endif()
+endif()
 
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
