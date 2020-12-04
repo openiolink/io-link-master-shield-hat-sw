cb90f8b94a787602790f549f794f0d33236a729f
Janik Lehmann <xxthegeckoxx@gmail.com>
2020-12-04 20:07:15 +0100

    removed unused commands

--- toolchains/toolchain-raspi.cmake
+++ toolchains/toolchain-raspi.cmake
@@ -1,10 +1,10 @@
 # https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-linux
 
+set(CMAKE_SYSROOT /home/janik/sysroot_local) #set the sysroot of your Raspberry Pi here
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_SYSROOT /home/janik/sysroot_local)
-set(CMAKE_STAGING_PREFIX /home/janik/sysroot/usr/local)
 set(TOOLCHAIN_BIN_PATH /usr/bin)
 
 set(CMAKE_C_COMPILER   ${TOOLCHAIN_BIN_PATH}/arm-linux-gnueabihf-gcc)
