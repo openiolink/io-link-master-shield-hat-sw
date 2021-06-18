62553f02b7301728e6011940dcdd49377fc038e4
Tobias Gammeter <gammt1@bfh.ch>
2021-06-18 22:59:24 +0200

    improved BicolorLed

--- src/board/BicolorLed.hpp
+++ src/board/BicolorLed.hpp
@@ -1,7 +1,34 @@
+//!*****************************************************************************
+//! \file   BicolorLED.hpp
+//!
+//! \author Tobias Gammeter (tobias.gammeter@gmail.com)
+//!
+//! \brief  Abstraction of a bicolor LED
+//!
+//! \date   2021-06-18
+//!
+//!
+//! ****************************************************************************
+//!
+//! \copyright
+//! Copyright 2021 Bern University of Applied Sciences and Balluff AG
+//! \n\n
+//! Licensed under the Apache License, Version 2.0 (the "License");
+//! you may not use this file except in compliance with the License.
+//! You may obtain a copy of the License at
+//! \n\n
+//!     http://www.apache.org/licenses/LICENSE-2.0
+//! \n\n
+//! Unless required by applicable law or agreed to in writing, software
+//! distributed under the License is distributed on an "AS IS" BASIS,
+//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//! See the License for the specific language governing permissions and
+//! limitations under the License.
+//!
+//!*****************************************************************************
+
 #ifndef OPENIOLINK_BICOLORLED_HPP
 #define OPENIOLINK_BICOLORLED_HPP
-
-// DRAFT file witch class BicolorLed
 
 #include "Pin_Arduino.hpp"
 #include "Pin_Raspberry.hpp"
@@ -10,30 +37,51 @@
 
 namespace openiolink
 {
-
-    // e.g. for port activity and/or port state
-    // to be used as type (no object)
+    //!*************************************************************************
+    //! \brief  Abstracion of a "bicolor" LED belonging to one port of an
+    //!         IO-Link Master. It may be use to visualize activity on the port
+    //!         and/or the state of the port.
+    //!
+    //! \note   This class is meant to be used for a green-red bicolor LED. Both
+    //!         color components are driven low-active via separate GPIOs.
+    //!
+    //! \note   This class should NOT be instantiated!
+    //!
+    //!*************************************************************************
     template <int IOLPortNr>
     class BicolorLed
     {
     public:
-        //
+        //!*********************************************************************
+        //! \brief  colors that a green-red bicolor LED can display
+        //!
+        //!*********************************************************************
         enum class LedColor
         {
-            OFF,
-            RED,
-            GREEN,
-            ORANGE
+            OFF,   // both colors off
+            RED,   // only red on
+            GREEN, // only green on
+            ORANGE // both colors on
         };
 
         static inline void init();
         static inline void setColor(const LedColor Color);
 
     private:
+        //! the GPIO for the red LED
         typedef HW::OutputPin<MapperIOLPort<IOLPortNr>::GreenLedPinNr> GreenLedPin;
+        //! the GPIO for the green LED
         typedef HW::OutputPin<MapperIOLPort<IOLPortNr>::RedLedPinNr> RedLedPin;
     };
 
+    //**************************************************************************
+    // Implementation of the inline Methods
+    //**************************************************************************
+
+    //!*************************************************************************
+    //! \brief  initialize the bicolor LED and switch it off
+    //!
+    //!*************************************************************************
     template <int IOLPortNr>
     inline void BicolorLed<IOLPortNr>::init()
     {
@@ -42,28 +90,35 @@ namespace openiolink
         setColor(LedColor::OFF);
     }
 
+    //!*************************************************************************
+    //! \brief  set the color of the "bicolor" LED
+    //!
+    //! \param Color [in]   the new color (enumerated value)
+    //!
+    //!*************************************************************************
     template <int IOLPortNr>
     inline void BicolorLed<IOLPortNr>::setColor(const LedColor Color)
     {
         switch (Color)
         {
-        case BicolorLed<IOLPortNr>::LedColor::RED:
+        case LedColor::RED:
+            GreenLedPin::setHigh(); // remember that the LEDs are low-active
+            RedLedPin::setLow();
+            break;
+        case LedColor::GREEN:
             GreenLedPin::setLow();
             RedLedPin::setHigh();
             break;
-        case LedColor::GREEN:
-            GreenLedPin::setHign();
+        case LedColor::ORANGE:
+            GreenLedPin::setLow();
             RedLedPin::setLow();
             break;
-        case LedColor::ORANGE:
+        case LedColor::OFF:
             GreenLedPin::setHigh();
             RedLedPin::setHigh();
             break;
-        case LedColor::OFF:
-            //[[falltrough]]
         default:
-            GreenLedPin::setLow();
-            RedLedPin::setLow();
+            static_assert(false, "Unsupported color for the BicolorLed requested!");
             break;
         }
     }
